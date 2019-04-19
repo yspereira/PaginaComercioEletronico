82f1d963f9c29fac06838dfaab33907d5b17e7d0
yspereira <39894010+yspereira@users.noreply.github.com>
2019-04-18 21:05:06 -0300

    Add files via upload

---
+++ ACComercioeletron/paginas/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def login(request):
+    return render(request,"login.html")
+
+def Index(request):
+    return render(request,"Index.html")
+
+def Eventos(request):
+    return render(request,"Eventos.html")
+
+def Tabeladeprecos(request):
+    return render(request,"Tabeladeprecos.html")
+
+def cadastroForm(request):
+    return render(request,"cadastroForm.html")
+
+
+
